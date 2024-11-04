0a459c7e3da4d1646128b986b621d57aa8e60f4e
Arjun Talwar <arjuntalwar1110@gmail.com>
2024-11-05 00:54:16 +0530

    new change

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 require("dotenv").config()
 const connectToMongo =require("./db");
-// connectToMongo();
+connectToMongo();
 const express = require("express")
-var fetchuser = require("./middleware/fetchUser")
 const app  = express();
 const port = 4000;
-const { body, validationResult } = require("express-validator");
-const bcrypt = require("bcryptjs")
-const User = require("./models/user")
-const Book = require("./models/book")
-var jwt = require("jsonwebtoken")
-const JWT_SECRET = process.env.JWT_SECRET
+
 const cors = require('cors');
 
 
@@ -21,6 +15,11 @@ app.use(express.json());
 app.get("/",(req,res)=>{
   res.json({message:"connection established"});
 })
+
+
+app.use('/auth',require('./routes/auth'));
+app.use('/book',require('./routes/book'));
+app.use('/user',require('./routes/user'));
 
 
 app.listen(port,()=>{
--- models/book.js
+++ models/book.js
@@ -5,12 +5,12 @@ const BookSchema = new Schema({
     owner: {type: mongoose.Schema.Types.ObjectId,ref: 'user'},
     title: { type: String, required: true },
     author: { type: String, required: true },
-    publicationDate: { type: Date, required: true },
     availability: { type: Boolean, default: true },
     genre: { type: String },
     description: { type: String },
     coverImageUrl: { type: String },
-    createdAt: { type: Date, default: Date.now }
+    createdAt: { type: Date, default: Date.now },
+    condition: {type : String,required: true}
 })
 
 const BookModel = model('Books', BookSchema);
