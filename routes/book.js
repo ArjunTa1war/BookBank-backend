d9ff5c38b1555925f1a1ea3a67c047d3f27d5a6b
Arjun Talwar <arjuntalwar1110@gmail.com>
2024-11-07 23:45:56 +0530

    change

--- routes/book.js
+++ routes/book.js
@@ -4,19 +4,76 @@ const Book = require("../models/book")
 
 /********************************* Find all books ***************************/
 
-router.get("/findBooks",async(req,res)=>{
-    const {tag,keyword} = req.body;
+router.get("/findAllBooks",async(req,res)=>{
     try {
-        // const book = await Boo;
+       const userId = req.body._id;
+       const books = await Book.find().lean();
+       const requestedBooks = books.map(book => ({
+        ...book,
+        isRequestedByUser: book.requestedBy && book.requestedBy.toString() === userId
+    }));
+    res.json(requestedBooks);
+    } catch (error) {
+        res.status(500).json({ message: 'Error fetching books', error });
+    }
+})
+
+/********************************* Get all books of user ***************************/
+
+router.get("/books/user/:userId",async(req,res)=>{
+    try {
+      const {type} = req.body;
+      const userId = req.params.userId;
+      let books;
+      if(type=="all"){
+        books = await Book.find({ owner: userId });
+      }else{
+        books = await Book.find({ requestedBy : userId});
+      }
+    res.json(books);
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching books', error });
+  }
+})
+
+/********************************* Add the books ***************************/
+
+router.post("/addBook",async(req,res)=>{
+    try {
+        
     } catch (error) {
         
     }
 })
 
-/********************************* Find all books ***************************/
-/********************************* Find all books ***************************/
-/********************************* Find all books ***************************/
-/********************************* Find all books ***************************/
-/********************************* Find all books ***************************/
+/********************************* Delete the book ***************************/
+
+router.delete("/deleteBook",async(req,res)=>{
+   try {
+    
+   } catch (error) {
+    
+   }
+})
+
+/********************************* Request Book ***************************/
+
+router.post("/requestBook",async(req,res)=>{
+   try {
+    
+   } catch (error) {
+    
+   }
+})
+
+/********************************* Give permission to request ***************************/
+
+router.post("/grantRequest",async(req,res)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+})
 
 module.exports = router;
